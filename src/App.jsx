beea726e1ff72005cf3571cf3dc75c8120103e81
TreasureUzoma <treasureuzoma650@gmail.com>
2025-04-19 08:21:47 +0100

    added vercel analytics to see its dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Nav from "./components/Nav";
 import Main from "./components/Main";
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
     return (
         <div>
             <Nav />
             <Main />
+            <Analytics />
         </div>
     );
 };
